eb61b1f232778e515e3c52bdb986dfee9f3fe807
8600913764 <patilakshay8649@gmail.com>
2026-03-12 18:02:55 +0530

    first-commit

---
+++ JDBC/src/akshay/connection/EstablishedConnectionJDBC.java
@@ -0,0 +1,29 @@
+package akshay.connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class EstablishedConnectionJDBC {
+
+    public static void main(String[] args) 
+    {
+    	System.out.println("EstablishedConnectionJDBC.main()");
+    	try {
+        	Class.forName("oracle.jdbc.driver.OracleDriver");
+            Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521/ORCL","system","patil123");
+
+            System.out.println("Connection Established Successfully");
+
+            con.close();
+
+            } 
+    	catch (Exception e) 
+    	{
+            e.printStackTrace();
+    	}    
+    }
+}
+
+// forName method:- we will use to provide oracle class name. belong from Class.
+//getConnection:- we will use provide id and pass 
