dd6818ba6710ae4b31f54cd613e42148869836b0
8600913764 <patilakshay8649@gmail.com>
2026-03-18 14:17:21 +0530

    commit

--- JDBC/src/akshay/connection/DemoRepo.java
+++ JDBC/src/akshay/connection/DemoRepo.java
@@ -4,6 +4,7 @@ public class DemoRepo {
 
 	public static void main(String[] args) {
 		System.out.println("demo repo");
+		System.out.println("DemoRepo.main()");
 		// TODO Auto-generated method stub
 
 	}
