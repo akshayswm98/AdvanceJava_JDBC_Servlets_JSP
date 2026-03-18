d744b1e1b5456e3348e16c5622b0002dca24844b
8600913764 <patilakshay8649@gmail.com>
2026-03-18 12:08:38 +0530

    first-commit

---
+++ JDBC/src/akshay/connection/DemoRepo.java
@@ -0,0 +1,11 @@
+package akshay.connection;
+
+public class DemoRepo {
+
+	public static void main(String[] args) {
+		System.out.println("demo repo");
+		// TODO Auto-generated method stub
+
+	}
+
+}
